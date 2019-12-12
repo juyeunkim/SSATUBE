1a28cb9880945746a47ed73b6f65c337ba151262
lllilllilllilili <hik3562@hufs.ac.kr>
2019-12-13 01:10:48 +0900

    feat: 🎸 server generate using express.js
    type get method
    
    BREAKING CHANGE: X
    
    Closes: #3

--- ingyu/index.js
+++ ingyu/index.js
@@ -1,8 +1,16 @@
-
 const express = require("express");
 const app = express();
 
 function handleListening(){
     console.log("Listening on: http://localhost:4002");
 }
-app.listen(4002, handleListening); 
+function handleHome(req, res){
+    console.log(req);
+    res.send('Hello from home');    
+}
+function handleProfile(req,res){
+    res.send("You are on my profile");
+}
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+app.listen(4007, handleListening); 
