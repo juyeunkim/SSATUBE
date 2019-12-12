1a89ca1a87749a9316ce1c0e67f78c8c55b16f64
lllilllilllilili <hik3562@hufs.ac.kr>
2019-12-12 23:50:54 +0900

    feat: 🎸 express test, server create
    npm install, express.js install and study
    
    BREAKING CHANGE: nothing
    
    Closes: #2

--- ingyu/index.js
+++ ingyu/index.js
@@ -1,1 +1,8 @@
-console.log('hi');
+
+const express = require("express");
+const app = express();
+
+function handleListening(){
+    console.log("Listening on: http://localhost:4002");
+}
+app.listen(4002, handleListening); 
