3714b45115fca93e75118896cbe5f182cc643f38
lllilllilllilili <hik3562@hufs.ac.kr>
2019-12-14 01:28:46 +0900

    feat: 🎸 babel
    .babelrc create, preset-stage0 install and using arrow function, use
    nodemon -D
    
    BREAKING CHANGE: X
    
    Closes: 4

--- ingyu/index.js
+++ ingyu/index.js
@@ -1,16 +1,14 @@
-const express = require("express");
+//const express = require("express");
+//babel 을 쓸려면 require 대신 import를 써야 한다. 
+import express from "express";
 const app = express();
 
-function handleListening(){
-    console.log("Listening on: http://localhost:4002");
-}
-function handleHome(req, res){
-    console.log(req);
-    res.send('Hello from home');    
-}
-function handleProfile(req,res){
-    res.send("You are on my profile");
-}
+const handleListening = () =>    console.log("Listening on: http://localhost:4002");
+
+const handleHome = (req, res) => res.send('Hello from home3');    
+
+const handleProfile = (req,res) =>     res.send("You are on my profile");
+
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
-app.listen(4007, handleListening); 
+app.listen(4012, handleListening); 
