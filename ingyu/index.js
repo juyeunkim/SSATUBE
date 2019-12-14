e87989ca2170c69227b44188d101bcc31c4fd1e6
lllilllilllilili <hik3562@hufs.ac.kr>
2019-12-14 16:58:01 +0900

    feat: 🎸 middleware(bodyParser, cookieParser)
    pracitce middleware
    
    BREAKING CHANGE: X
    
    Closes: 7

--- ingyu/index.js
+++ ingyu/index.js
@@ -3,6 +3,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
 
@@ -13,11 +15,14 @@ const handleHome = (req, res) => res.send('Hello from home3');
 const handleProfile = (req,res) =>     res.send("You are on my profile");
 app.use(helmet()); //보안을 위한것이다. 
 app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(bodyParser().json());
+app.use(bodyParser().urlencoded({extended : true})); //위에서부터 아래로 
 
 const middleware = (req,res,next) => {
     res.send('not happending');
 }
-
+//route 
 app.get("/",middleware, handleHome); //middleware 로 연결이 끊킨다. 
 app.get("/profile", handleProfile);
 
