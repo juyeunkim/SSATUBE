f8d595bd9106ba8be50a3cf9757fcb5cbfc8135e
lllilllilllilili <hik3562@hufs.ac.kr>
2019-12-14 16:47:59 +0900

    feat: 🎸 middleware-2
    disconnect using middleware
    
    BREAKING CHANGE: X
    
    Closes: 6

--- ingyu/index.js
+++ ingyu/index.js
@@ -2,6 +2,8 @@
 //babel 을 쓸려면 require 대신 import를 써야 한다. 
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
+
 const app = express();
 
 const handleListening = () =>    console.log("Listening on: http://localhost:4002");
@@ -9,14 +11,15 @@ const handleListening = () =>    console.log("Listening on: http://localhost:400
 const handleHome = (req, res) => res.send('Hello from home3');    
 
 const handleProfile = (req,res) =>     res.send("You are on my profile");
+app.use(helmet()); //보안을 위한것이다. 
+app.use(morgan("dev"));
 
-const betweenHome = (req, res, next) =>{
-    console.log('Between');
-    next(); //다음 미들웨어 호출 
+const middleware = (req,res,next) => {
+    res.send('not happending');
 }
-app.use(betweenHome); //여기다 둬야 미들웨어 로서 역할을 다할 수 있다. 
-app.get("/", betweenHome, handleHome);
+
+app.get("/",middleware, handleHome); //middleware 로 연결이 끊킨다. 
 app.get("/profile", handleProfile);
 
 
-app.listen(4012, handleListening); 
+app.listen(4014, handleListening); 
