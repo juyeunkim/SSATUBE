2b884b7525988af7bc5545b051419f7802017a6e
lllilllilllilili <hik3562@hufs.ac.kr>
2019-12-14 16:33:51 +0900

    test: 💍 middleware
    practice middleware function
    
    BREAKING CHANGE: X
    
    Closes: 5

--- ingyu/index.js
+++ ingyu/index.js
@@ -1,6 +1,7 @@
 //const express = require("express");
 //babel 을 쓸려면 require 대신 import를 써야 한다. 
 import express from "express";
+import morgan from "morgan";
 const app = express();
 
 const handleListening = () =>    console.log("Listening on: http://localhost:4002");
@@ -9,6 +10,13 @@ const handleHome = (req, res) => res.send('Hello from home3');
 
 const handleProfile = (req,res) =>     res.send("You are on my profile");
 
-app.get("/", handleHome);
+const betweenHome = (req, res, next) =>{
+    console.log('Between');
+    next(); //다음 미들웨어 호출 
+}
+app.use(betweenHome); //여기다 둬야 미들웨어 로서 역할을 다할 수 있다. 
+app.get("/", betweenHome, handleHome);
 app.get("/profile", handleProfile);
+
+
 app.listen(4012, handleListening); 
